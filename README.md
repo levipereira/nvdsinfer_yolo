dc70154aba46ceabde4a1bf77c65330fd9414fd4
Levi Pereira <levi.pereira@gmail.com>
2024-05-09 20:41:29 -0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,56 @@
+# NvDsInferYoloEfficientNMS for Gst-nvinferserver
+
+This repository provides a custom implementation of parsing function to the [Gst-nvinferserver](https://docs.nvidia.com/metropolis/deepstream/dev-guide/text/DS_plugin_gst-nvinferserver.html) plugin when use YOLOv7/YOLOv9 model served by Triton Server using the [Efficient NMS](https://github.com/NVIDIA/TensorRT/tree/master/plugin/efficientNMSPlugin) plugin exported by ONNX.
+
+
+By using the parsing function provided by `NvDsInferYoloEfficientNMS`, handling the number of classes dynamically becomes easier. This eliminates the need to hardcode the number of classes, allowing the same plugin to be used for different YOLOv9 models with varying numbers of classes.
+
+- Use parse function `NvDsInferYoloEfficientNMS` for YOLO Exported ONNX model with End2End
+
+
+# Deployment Guide for NvDsInferYoloEfficientNMS
+
+
+## Cloning Repository and Installation
+
+To clone the repository and build\install the `libnvds_infer_yolo_efficient_nms.so` library, follow these steps:
+
+
+```bash
+# Clone the repository
+git clone https://github.com/levipereira/nvdsinfer_yolo_efficient_nms.git
+
+# Copy the repository to the desired location
+cp -R nvdsinfer_yolo_efficient_nms/ /opt/nvidia/deepstream/deepstream/sources/libs/
+
+# Set the CUDA_VER environment variable (check your deepstream cuda version.  The DS 6.4 use cuda 12.2)
+export CUDA_VER=12.2
+
+# Navigate to the directory containing the nvdsinfer_yolov9_efficient_nms library
+cd /opt/nvidia/deepstream/deepstream/sources/libs/nvdsinfer_yolo_efficient_nms
+
+# Build the project using the provided MakeFile
+make -f MakeFile all
+make -f MakeFile install
+
+```
+Install Location:
+
+`/opt/nvidia/deepstream/deepstream/lib/libnvds_infer_yolo_efficient_nms.so`
+
+Usage on Deepstream
+
+Snippet [Gst-nvinferserver](https://docs.nvidia.com/metropolis/deepstream/dev-guide/text/DS_plugin_gst-nvinferserver.html)  Configuration File for YOLO Models
+```
+  postprocess {
+    labelfile_path: "labels.txt"
+    detection {
+      num_detected_classes: 80
+      custom_parse_bbox_func: "NvDsInferYoloEfficientNMS"
+    }
+  }
+  custom_lib {
+    path : "/opt/nvidia/deepstream/deepstream/lib/libnvds_infer_yolo_efficient_nms.so"
+  }
+```
+
---
+++ nvdsinfer_yolo_nms.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2018-2020, NVIDIA CORPORATION. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+
+
+
+ * Modifications made by Levi Pereira (https://forums.developer.nvidia.com/u/levi_pereira/activity):
+ *  - Customized the function NvDsInferParseCustomEfficientNMS by creating the NvDsInferYoloEfficientNMS.
+ */
+
+
+
+#include <cstring>
+#include <iostream>
+#include "nvdsinfer_custom_impl.h"
+#include <cassert>
+#include <cmath>
+
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define CLIP(a,min,max) (MAX(MIN(a, max), min))
+
+
+/* C-linkage to prevent name-mangling */
+extern "C"
+bool NvDsInferYoloNMS (std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
+                                   NvDsInferNetworkInfo  const &networkInfo,
+                                   NvDsInferParseDetectionParams const &detectionParams,
+                                   std::vector<NvDsInferObjectDetectionInfo> &objectList);
+                                   
+
+extern "C"
+bool NvDsInferYoloNMS (std::vector<NvDsInferLayerInfo> const &outputLayersInfo,
+                                   NvDsInferNetworkInfo  const &networkInfo,
+                                   NvDsInferParseDetectionParams const &detectionParams,
+                                   std::vector<NvDsInferObjectDetectionInfo> &objectList) {
+    if(outputLayersInfo.size() != 5)
+    {
+        std::cerr << "Mismatch in the number of output buffers."
+                  << "Expected 4 output buffers, detected in the network :"
+                  << outputLayersInfo.size() << std::endl;
+        return false;
+    }
+
+    auto layerFinder = [&outputLayersInfo](const std::string &name)
+        -> const NvDsInferLayerInfo *{
+        for (auto &layer : outputLayersInfo) {
+            if (layer.layerName && name == layer.layerName) {
+                return &layer;
+            }
+        }
+        return nullptr;
+    };
+
+    const NvDsInferLayerInfo *num_detsLayer = layerFinder("num_dets");
+    const NvDsInferLayerInfo *boxesLayer = layerFinder("det_boxes");
+    const NvDsInferLayerInfo *scoresLayer = layerFinder("det_scores");
+    const NvDsInferLayerInfo *classesLayer = layerFinder("det_classes");
+    const NvDsInferLayerInfo *indicesLayer = layerFinder("det_indices");
+
+    if (!num_detsLayer || !boxesLayer || !scoresLayer || !classesLayer || !indicesLayer) {
+        if (!num_detsLayer) {
+            std::cerr << "  - num_detsLayer: Missing or unsupported data type." << std::endl;
+        }
+
+        if (!boxesLayer) {
+            std::cerr << "  - boxesLayer: Missing or unsupported data type." << std::endl;
+        }
+
+        if (!scoresLayer) {
+            std::cerr << "  - scoresLayer: Missing or unsupported data type." << std::endl;
+        }
+
+        if (!classesLayer) {
+            std::cerr << "  - classesLayer: Missing or unsupported data type." << std::endl;
+        }
+
+        if (!indicesLayer) {
+            std::cerr << "  - indicesLayer: Missing or unsupported data type." << std::endl;
+        }
+        return false;
+    }
+
+    if(num_detsLayer->inferDims.numDims != 1U) {
+        std::cerr << "Network num_dets dims is : " <<
+            num_detsLayer->inferDims.numDims << " expect is 1"<< std::endl;
+        return false;
+    }
+    if(boxesLayer->inferDims.numDims != 2U) {
+        std::cerr << "Network det_boxes dims is : " <<
+            boxesLayer->inferDims.numDims << " expect is 2"<< std::endl;
+        return false;
+    }
+    if(scoresLayer->inferDims.numDims != 1U) {
+        std::cerr << "Network det_scores dims is : " <<
+            scoresLayer->inferDims.numDims << " expect is 1"<< std::endl;
+        return false;
+    }
+    if(classesLayer->inferDims.numDims != 1U) {
+        std::cerr << "Network det_classes dims is : " <<
+            classesLayer->inferDims.numDims << " expect is 1"<< std::endl;
+        return false;
+    }
+    if(indicesLayer->inferDims.numDims != 1U) {
+        std::cerr << "Network det_indices dims is : " <<
+            indicesLayer->inferDims.numDims << " expect is 1"<< std::endl;
+        return false;
+    }
+
+    const char* log_enable = std::getenv("ENABLE_DEBUG");
+
+    int* p_keep_count = (int *) num_detsLayer->buffer;
+    int* p_indices = (int *) indicesLayer->buffer;
+    float* p_bboxes = (float *) boxesLayer->buffer;
+
+    NvDsInferDims inferDims_p_bboxes = boxesLayer->inferDims;
+    int numElements_p_bboxes=inferDims_p_bboxes.numElements;
+
+    float* p_scores = (float *) scoresLayer->buffer;
+    unsigned int* p_classes = (unsigned int *) classesLayer->buffer;
+    const float threshold = detectionParams.perClassThreshold[0];
+
+    float max_bbox=0;
+    for (int i=0; i < numElements_p_bboxes; i++)
+    {
+        if ( max_bbox < p_bboxes[i] )
+            max_bbox=p_bboxes[i];
+    }
+
+    if (p_keep_count[0] > 0)
+    {
+        assert (!(max_bbox < 2.0));
+        for (int i = 0; i < p_keep_count[0]; i++) {
+
+            if ( p_scores[i] < threshold) continue;
+            if ((unsigned int) p_classes[i] >= detectionParams.numClassesConfigured) {
+                printf("Error: The number of classes configured in the GIE config-file (postprocess > num_detected_classes) is incorrect.\n");
+                printf("Detected class index: %u\n", (unsigned int) p_classes[i]);
+            }
+            assert((unsigned int) p_classes[i] < detectionParams.numClassesConfigured);
+
+            NvDsInferObjectDetectionInfo object;
+            object.classId = (int) p_classes[i];
+            object.detectionConfidence = p_scores[i];
+
+            object.left=p_bboxes[4*i];
+            object.top=p_bboxes[4*i+1];
+            object.width=(p_bboxes[4*i+2] - object.left);
+            object.height= (p_bboxes[4*i+3] - object.top);
+
+            if(log_enable != NULL && std::stoi(log_enable)) {
+                std::cout << "idx/label/conf/ x/y w/h -- "
+                << p_indices[i] << " "
+                << p_classes[i] << " "
+                << p_scores[i] << " "
+                << object.left << " " << object.top << " " << object.width << " "<< object.height << " "
+                << std::endl;
+            }
+
+            object.left=CLIP(object.left, 0, networkInfo.width - 1);
+            object.top=CLIP(object.top, 0, networkInfo.height - 1);
+            object.width=CLIP(object.width, 0, networkInfo.width - 1);
+            object.height=CLIP(object.height, 0, networkInfo.height - 1);
+
+            objectList.push_back(object);
+        }
+    }
+    return true;
+}
+
+CHECK_CUSTOM_PARSE_FUNC_PROTOTYPE(NvDsInferYoloNMS);
